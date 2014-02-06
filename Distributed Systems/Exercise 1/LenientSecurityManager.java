4d98fe7575bf0dbd8cea5e23fd6546c1f1752266
Even Lislebø <e.lislebo@gmail.com>
2014-02-06 14:34:38 +0100

    Minor

--- Distributed Systems/Exercise 1/LenientSecurityManager.java
+++ Distributed Systems/Exercise 1/LenientSecurityManager.java
@@ -5,7 +5,7 @@ public class LenientSecurityManager extends SecurityManager {
     private Hashtable grantedPermissions;
 
     public LenientSecurityManager(){
-	permissions = new HashSet();
+	grantedPermissions = new Hashtable();
     }
 
     public void checkPermission(Permission perm) {
